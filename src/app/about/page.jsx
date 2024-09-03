b9202f62ebb7e54f311199ff9b216f8a2482d995
Arunraj003 <arunr6506@gmail.com>
2024-09-03 11:24:55 +0530

    commit "transition added"

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client";
+
+import { motion } from "framer-motion"
+import React from "react"
+
 
 const AboutPage = () => {
     return (
-        <div>page</div>
+        <motion.div
+            className="h-full"
+            initial={{ y: "-200vh" }}
+            animate={{ y: "0%" }}
+            transition={{ duration: 1 }}>
+            {/* container  */}
+            <div className="">
+                {/* text section */}
+                <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+                    <div className="">
+                        BIOGRAPHY
+                    </div>
+                    <div className="">
+                        SKILLS
+                    </div>
+                    <div className="">
+                        EXPERIENCE
+                    </div>
+                </div>
+                {/* svg section */}
+            </div>
+        </motion.div>
     )
 }
 
--- src/app/components/transitionProvider.jsx
+++ src/app/components/transitionProvider.jsx
@@ -18,7 +18,7 @@ const TransitionProvider = ({ children }) => {
           className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
           exit={{ height: "140vh" }}
-          transition={{ duration: 0.5, ease: "easeOut" }}
+          transition={    { duration: 0.5, ease: "easeOut" }}
         />
         <motion.div
           className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
